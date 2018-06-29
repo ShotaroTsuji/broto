75c276ae1bbd84f7eb0bdc96ce930aed8fd4c32c
Shotaro Tsuji <Shotaro.Tsuji@gmail.com>
2018-06-29 18:47:05 +0900

    Implemented trait Error for enum Error

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,54 @@
-use std;
+use std::io;
+use std::string;
+use std::fmt;
+use std::result;
+use std::error;
 
 #[derive(Debug)]
 pub enum Error {
     EndOfFile,
     Magic,
     UndefinedBlock,
-    Io(std::io::Error),
-    FromUtf8(std::string::FromUtf8Error),
+    Io(io::Error),
+    FromUtf8(string::FromUtf8Error),
 }
 
-impl From<std::io::Error> for Error {
-    fn from(err: std::io::Error) -> Self {
+pub type Result<T> = result::Result<T, Error>;
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
         Error::Io(err)
     }
 }
 
-pub type Result<T> = std::result::Result<T, Error>;
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::EndOfFile => write!(f, "End of File"),
+            Error::Magic => write!(f, "Magic number error"),
+            Error::UndefinedBlock => write!(f, "Undefined Block"),
+            Error::Io(ref err) => write!(f, "IO error: {}", err),
+            Error::FromUtf8(ref err) => write!(f, "String error: {}", err),
+        }
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::EndOfFile => "End of File",
+            Error::Magic => "Magic number",
+            Error::UndefinedBlock => "UndefinedBlock",
+            Error::Io(ref err) => err.description(),
+            Error::FromUtf8(ref err) => err.description(),
+        }
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        match *self {
+            Error::Io(ref err) => Some(err),
+            Error::FromUtf8(ref err) => Some(err),
+            _ => None,
+        }
+    }
+}
